5c6dafc35ebdf96d900228fe9f714d4d0e78709f
leokojima <leokojima92@gmail.com>
2024-01-17 17:37:36 -0300

    changing global exception handler to notfound

--- src/main/java/com/example/restservice/exceptions/GlobalControllerExceptionHandler.java
+++ src/main/java/com/example/restservice/exceptions/GlobalControllerExceptionHandler.java
@@ -1,6 +1,6 @@
 package com.example.restservice.exceptions;
 
-import org.springframework.core.convert.ConversionFailedException;
+import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -10,10 +10,10 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalControllerExceptionHandler {
 
-    @ExceptionHandler(ConversionFailedException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ResponseEntity<String> handleConversion(RuntimeException ex) {
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    @ExceptionHandler(NotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ResponseEntity<String> handleNotFound(RuntimeException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
     }
 
 }
