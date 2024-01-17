1a99c42bfba20dd5c9ba0921f2c978b9567bf793
leokojima <leokojima92@gmail.com>
2024-01-17 15:40:24 -0300

    add readme and remove debugging log

--- README.md
+++ README.md
@@ -17,8 +17,10 @@ java -jar target/rest-service-0.0.1-SNAPSHOT.jar
 
 ## Honorable Branch Mentions
 
-### h2-memorydb
+### swagger-ui
 
 1. Only GetAll works
 2. You can configure not to add some initial lines to h2 in application.properties
-3. Class CreditCardCommandLineRunner is a simple implementation to visualize said initial lines on when debugging
+3. removed Class CreditCardCommandLineRunner, which is a simple implementation to visualize said initial lines on when debugging
+4. added swagger ui
+5. switch from static variable repository to JPA repository
--- src/main/java/com/example/restservice/RestServiceApplication.java
+++ src/main/java/com/example/restservice/RestServiceApplication.java
@@ -1,33 +1,13 @@
 package com.example.restservice;
 
-import com.example.restservice.models.CreditCard;
-import com.example.restservice.repositories.CreditCardRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class RestServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RestServiceApplication.class, args);
-    }
-
-}
-
-//Just to print what is already in db
-@Component
-class CreditCardCommandLineRunner implements CommandLineRunner {
-
-    @Autowired CreditCardRepository creditCardRepository;
-
-    @Override
-    public void run(String... args) throws Exception {
-
-        for (CreditCard c : creditCardRepository.findAll())
-            System.out.println(c.toString());
     }
 
 }
