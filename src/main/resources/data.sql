647eb5f43b39fd2967a8c70f126ca35e6f6553dc
leokojima <leokojima92@gmail.com>
2024-01-17 20:29:21 -0300

    simplifying db initialization

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,4 @@
 INSERT INTO credit_card (card_number, card_holder, card_expiration_date)
 VALUES
 ('1234123412341234', 'LeoTK', '12/23'),
-('2222222222222222', 'LeoTK', '12/23'),
-('3333333333333333', 'LeoTK', '12/23'),
-('4444444444444444', 'LeoTK', '12/23'),
-('5555555555555555', 'LeoTK', '12/23');
+('2222222222222222', 'LeoTK', '12/23');
